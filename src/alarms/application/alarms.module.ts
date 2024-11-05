a867469217e07a5a2e6dc02a8c7f8819f7f9d9f5
“sherySJ” <sherjan909@gmail.com>
2024-11-05 21:23:07 +0500

    add initial CQRS  support for alarm management with command and query handlers

--- src/alarms/application/alarms.module.ts
+++ src/alarms/application/alarms.module.ts
@@ -1,11 +1,18 @@
 import { DynamicModule, Module, Type } from "@nestjs/common";
-import { AlarmsService } from "./alarms.service";
-import { AlarmsController } from "../presenters/http/alarms.controller";
 import { AlarmFactory } from "../domain/factories/alarm.factory";
+import { AlarmsController } from "../presenters/http/alarms.controller";
+import { AlarmsService } from "./alarms.service";
+import { CreateAlarmCommandHandler } from "./commands/create-alarm-command";
+import { GetAlarmsQueryHandler } from "../queries/get-alarms.query-handler";
 
 @Module({
   controllers: [AlarmsController],
-  providers: [AlarmsService, AlarmFactory],
+  providers: [
+    AlarmsService,
+    AlarmFactory,
+    CreateAlarmCommandHandler,
+    GetAlarmsQueryHandler,
+  ],
 })
 export class AlarmsModule {
   static withInfrasturce(infrastructureModule: Type | DynamicModule) {
--- src/alarms/application/commands/create-alarm-command.ts
+++ src/alarms/application/commands/create-alarm-command.ts
@@ -1,6 +1,25 @@
-export class CreateAlarmCommand {
+import { CommandHandler, ICommandHandler } from "@nestjs/cqrs";
+import { CreateAlarmCommand } from "./create-alarm-command-handler";
+import { Logger } from "@nestjs/common";
+import { AlarmRepository } from "../ports/alarm.repository";
+import { AlarmFactory } from "src/alarms/domain/factories/alarm.factory";
+
+@CommandHandler(CreateAlarmCommand)
+export class CreateAlarmCommandHandler
+  implements ICommandHandler<CreateAlarmCommand>
+{
+  private readonly logger = new Logger(CreateAlarmCommandHandler.name);
+
   constructor(
-    public readonly name: string,
-    public readonly severity: string,
+    private readonly alarmRepository: AlarmRepository,
+    private readonly alarmFactory: AlarmFactory,
   ) {}
+  async execute(command: CreateAlarmCommand) {
+    this.logger.debug(
+      `Processing "CreateAlarmCommand": ${JSON.stringify(command)} command...`,
+    );
+    const alarm = this.alarmFactory.create(command.name, command.severity);
+    return this.alarmRepository.save(alarm);
+  }
 }
+export { CreateAlarmCommand };
